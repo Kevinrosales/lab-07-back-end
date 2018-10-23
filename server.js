ccf2100aeb1fd53e4a3cba940c5292772b718b48
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-10-23 11:41:48 -0700

    we done dided the done

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/location', (request,response) => {
 function searchToLatLong(query) {
   const geoData = require('./data/geo.json');
   const location = new Location(geoData.results[0]);
-  location.search_query = query;
+  // location.search_query = query;
   return location;
 }
 
@@ -29,9 +29,30 @@ function Location(data) {
   this.longitude = data.geometry.location.lng;
 }
 
+app.get('/weather', (request,response) => {
+  const forcastData = searchWeatherData(request.query.data);
+  response.send(forcastData);
+});
 
+function searchWeatherData(query){
+  const weatherData = require('./data/darksky.json');
+  const dailyWeather = [];
+  const weather = weatherData.daily.data.forEach((item)=>{
+    dailyWeather.push(new Weather(item));
+  });
+  // weather.search_query = query;
+  console.log(dailyWeather);
+  return dailyWeather;
+}
 
+function Weather(data){
+  this.time = data.time;
+  this.forecast = data.summary;
+  console.log(data.summary);
+}
 
-
+app.get('/', function (request, response){
+  throw new Error('status: 500', 'Sorry, somthing went wrong.')
+})
 
 app.listen(PORT, () => console.log(`App is up on ${PORT}`));
