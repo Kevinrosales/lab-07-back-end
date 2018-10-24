62f335f4e1ff870e667f7b39751a9a8d6267c142
Charles Clemens <cclemensjr@gmail.com>
2018-10-24 10:13:53 -0700

    Got google maps working based on live data

--- server.js
+++ server.js
@@ -2,25 +2,34 @@
 
 const express = require('express');
 const cors = require('cors');
-
+const superagent = require('superagent');
 require('dotenv').config();
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(cors());
 
+/*************    LOCATION API  *******************/
+
 app.get('/location', (request,response) => {
-  const locationData = searchToLatLong(request.query.data);
-  response.send(locationData);
+  getLocation(request.query.data)
+    .then( locationData => response.send(locationData) )
+    .catch( error => handleError(error, response) );
 });
 
-function searchToLatLong(query) {
-  const geoData = require('./data/geo.json');
-  const location = new Location(geoData.results[0]);
 
-  return location;
+function getLocation(query) {
+
+  const _URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  return superagent.get(_URL)
+    .then( data => {
+      if ( ! data.body.results.length ) { throw 'No Data'; }
+      else {
+        let location = new Location(data.body.results[0]);
+        location.search_query = query;
+        return location;
+      }
+    });
 }
 
 function Location(data) {
@@ -28,6 +37,27 @@ function Location(data) {
   this.latitude = data.geometry.location.lat;
   this.longitude = data.geometry.location.lng;
 }
+
+// app.get('/location', (request,response) => {
+//   const locationData = searchToLatLong(request.query.data);
+//   response.send(locationData);
+// });
+
+// function searchToLatLong(query) {
+//   const geoData = require('./data/geo.json');
+//   const location = new Location(geoData.results[0]);
+
+//   return location;
+// }
+
+// function Location(data) {
+//   this.formatted_query = data.formatted_address;
+//   this.latitude = data.geometry.location.lat;
+//   this.longitude = data.geometry.location.lng;
+// }
+
+
+/*************    WEATHER API  *******************/
 
 app.get('/weather', (request,response) => {
   const forcastData = searchWeatherData(request.query.data);
@@ -51,8 +81,9 @@ function Weather(data){
 }
 
 
-app.get('/', function (request, response){
-  throw new Error('status: 500', 'Sorry, somthing went wrong.')
-})
+function handleError(err, res) {
+  console.error('ERR', err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
 
 app.listen(PORT, () => console.log(`App is up on ${PORT}`));
